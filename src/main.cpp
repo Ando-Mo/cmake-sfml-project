bb948c9cf7258560927b60af9216d7104947f565
Andrew Mangal <amangal@ucsc.edu>
2023-12-12 18:08:01 -0800

    Beginning the Suika project with borrowed verlet code

--- src/main.cpp
+++ src/main.cpp
@@ -1,546 +1,75 @@
-#include <SFML/Graphics.hpp>
-#include <SFML/Window.hpp>
-#include <SFML/Audio.hpp>
-#include <SFML/Network.hpp>
-#include <SFML/System.hpp>
-#include <SFML/OpenGL.hpp>
-#include <SFML/Main.hpp>
 #include <iostream>
-#include <ostream>
-class Particle {
-public:
-	sf::Vector2f pixelPos;
-	sf::Vector2f pixelVelocity;
-	float pixelRadius;
-	float timeToLive;
-	sf::Color color;
+#include <SFML/Graphics.hpp>
+#include "solver.hpp"
+#include "renderer.hpp"
+#include "utils/number_generator.hpp"
+#include "utils/math.hpp"
 
-	Particle() {
-		pixelRadius = 32.f;
-		timeToLive = 1.f;
 
-		pixelPos = sf::Vector2f(0, 0);
-		pixelVelocity = sf::Vector2f(0, 0);
-
-		color = sf::Color::White;
-	}
-
-	Particle(float pixelRadius, sf::Vector2f initPos, sf::Vector2f initVelocity, float time, sf::Color newCol) {
-		this->pixelPos = initPos;
-		this->pixelVelocity = initVelocity;
-		this->pixelRadius = pixelRadius;
-		this->timeToLive = time;
-		this->color = newCol;
-	}
-
-	sf::CircleShape getShape() {
-		auto shape = sf::CircleShape();
-		shape.setRadius(pixelRadius);
-		shape.setPosition(pixelPos);
-		shape.setFillColor(color);
-		return shape;
-	}
-
-	void update(float dt) {
-		pixelPos += pixelVelocity * dt;
-		timeToLive -= dt;
-	}
-
-	bool isDead() {
-		return timeToLive <= 0;
-	}
-
-};
-class Ball {
-public:
-	sf::Vector2f pos;
-	sf::Vector2f velocity;
-	sf::Vector2f size;
-	sf::Color color;
-
-	Ball() { //TEST: ball moves to the right?
-		size = sf::Vector2f(20, 20);
-
-		pos = sf::Vector2f(0, 250);
-		velocity = sf::Vector2f(200, 40);
-
-		color = sf::Color::Blue;
-	}
-
-	Ball(sf::Vector2f newSize, sf::Vector2f initPos, sf::Vector2f initVelocity, sf::Color newCol) {
-		this->pos = initPos;
-		this->velocity = initVelocity;
-		this->size = newSize;
-		this->color = newCol;
-	}
-
-	sf::RectangleShape getShape() {
-		auto shape = sf::RectangleShape();
-		shape.setSize(size);
-		shape.setPosition(pos);
-		shape.setFillColor(color);
-		return shape;
-	}
-
-	void update(float dt) {
-		pos += velocity * dt;
-        checkCollisions();
-	}
-
-    void checkCollisions(){
-        //is ball touching left paddle?
-        //is ball touching right paddle?
-        //is ball touching up wall?
-        //is ball touching down wall?
-            //then figure out which side is touching, and reverse direction vector, and reflect the angle accordingly
-
-        //is ball touching left wall?
-        //is ball touching right wall?
-            //then change game state
-            //make ball disappear
-            //flash score for 1 second
-            //place ball on center screen for half second
-            //then start game again 
-    }
-
-};
-class Paddle {
-
-public:
-	sf::Vector2f pos;
-	sf::Vector2f velocity;
-	sf::Vector2f size;
-	sf::Color color;
-
-	Paddle() { //TEST: ball moves to the right?
-		size = sf::Vector2f(15, 120);
-
-		pos = sf::Vector2f(300, 250);
-		velocity = sf::Vector2f(0, 0);
-
-		color = sf::Color::Yellow;
-	}
-
-	Paddle(sf::Vector2f newSize, sf::Vector2f initPos, sf::Vector2f initVelocity, sf::Color newCol) {
-		this->pos = initPos;
-		this->velocity = initVelocity;
-		this->size = newSize;
-		this->color = newCol;
-	}
-
-	sf::RectangleShape getShape() {
-		auto shape = sf::RectangleShape();
-		shape.setSize(size);
-		shape.setPosition(pos);
-		shape.setFillColor(color);
-		return shape;
-	}
-
-	void update(float dt, float dir) {
-		pos += ((velocity * dir) * dt);
-	}
-};
-class Wall {
-public:
-	sf::Vector2f pos;
-	sf::Vector2f size;
-	sf::Color color;
-
-	Wall() { //TEST: ball moves to the right?
-		size = sf::Vector2f(15, 120);
-
-		pos = sf::Vector2f(300, 250);
-        
-		color = sf::Color::Red;
-	}
-
-	Wall(sf::Vector2f newSize, sf::Vector2f initPos, sf::Color newCol) {
-		this->pos = initPos;
-		this->size = newSize;
-		this->color = newCol;
-	}
-
-	sf::RectangleShape getShape() {
-		auto shape = sf::RectangleShape();
-		shape.setSize(size);
-		shape.setPosition(pos);
-		shape.setFillColor(color);
-		return shape;
-	}
-};
-
-enum State{
-    player_press_enter,
-    player_num_choose,
-
-    ball_prerelease, 
-    ball_moving_regular_gameplay, 
-    ball_hit_wall_so_flash,
-
-	PAUSE,
-};
-
-enum Mode{
-    OnePlayerMode, 
-    TwoPlayerMode,
-};
-
-bool intersectRects(sf::FloatRect& r1, sf::FloatRect& r2){ //DOES NOT WORK, SHOULD BE AND -- DOESN'T TAKE BOTH X AND Y INTO ACCOUNT
-    // return !(r1.left + r1.width <= r2.left ||
-    //          r1.left >= r2.left + r2.width || 
-    //          r1.top + r1.height <= r2.top ||
-    //          r1.top >= r2.top + r2.height); 
-    
-    //std::cout << "X: " << r1.left << "Y: " << r1.top << "Width: " << r1.width << "Height" << r1.height << std::endl;
-    if (r1.left + r1.width >= r2.left &&    // r1 right edge past r2 left
-        r1.left <= r2.left + r2.width &&    // r1 left edge past r2 right
-        r1.top + r1.height >= r2.top &&    // r1 top edge past r2 bottom
-        r1.top <= r2.top + r2.height) {    // r1 bottom edge past r2 top
-        return true;
-  }
-  return false;
-}
-int main()
+static sf::Color getRainbow(float t)
 {
-	sf::RenderWindow window(sf::VideoMode(1000, 1000), "PNOG"); //set window
+    const float r = sin(t);
+    const float g = sin(t + 0.33f * 2.0f * Math::PI);
+    const float b = sin(t + 0.66f * 2.0f * Math::PI);
+    return {static_cast<uint8_t>(255.0f * r * r),
+            static_cast<uint8_t>(255.0f * g * g),
+            static_cast<uint8_t>(255.0f * b * b)};
+}
 
-	sf::Clock deltaSrc;
-	bool mouseDown = false;
 
-    int p1Score = 0; 
-    int p2Score = 0; 
+int32_t main(int32_t, char*[])
+{
+    // Create window
+    constexpr int32_t window_width  = 1000;
+    constexpr int32_t window_height = 1000;
 
-    auto ballSize = sf::Vector2f(20, 20);
-    auto ballPos = sf::Vector2f(480, 450);
-	auto ballVelocity = sf::Vector2f(200, 100);
-    auto ballColor = sf::Color::White;
-    Ball ball = Ball(ballSize, ballPos, ballVelocity, ballColor);
-    
-    auto paddleSize = sf::Vector2f(5, 120);
-    auto paddleVelocity = sf::Vector2f(0, 200);
-    auto paddleColor = sf::Color::Red;
-	auto paddle1_pos = sf::Vector2f(150, 400);
-	auto paddle2_pos = sf::Vector2f(800, 400);
-    Paddle paddle1 = Paddle(paddleSize, paddle1_pos, paddleVelocity, paddleColor);
-    Paddle paddle2 = Paddle(paddleSize, paddle2_pos, paddleVelocity, paddleColor);
-    
-    Wall upWall = Wall(sf::Vector2f(1000, 40), sf::Vector2f(0,100), sf::Color::Magenta);
-    Wall downWall = Wall(sf::Vector2f(1000, 40), sf::Vector2f(0,800), sf::Color::Magenta);
+    sf::ContextSettings settings;
+    settings.antialiasingLevel = 1;
+    sf::RenderWindow window(sf::VideoMode(window_width, window_height), "Verlet", sf::Style::Default, settings);
+    const uint32_t frame_rate = 60;
+    window.setFramerateLimit(frame_rate);
 
-    Wall leftWall = Wall(sf::Vector2f(50, 800), sf::Vector2f(50, 0), sf::Color::Green); 
-    Wall rightWall = Wall(sf::Vector2f(50, 800), sf::Vector2f(900, 0), sf::Color::Green); 
+    Solver   solver;
+    Renderer renderer{window};
 
-	Wall flashSquare = Wall(sf::Vector2f(1000, 100), sf::Vector2f(0, 0), sf::Color::Yellow);
-	float flashTime = 1.0f;
+    // Solver configuration
+    solver.setConstraint({static_cast<float>(window_width) * 0.5f, static_cast<float>(window_height) * 0.5f}, 450.0f);
+    solver.setSubStepsCount(8);
+    solver.setSimulationUpdateRate(frame_rate);
 
-    bool paddleTouch = false;
-    bool ceilingTouch = false;
-    bool leftWallTouch = false;
-    bool rightWallTouch = false;
+    // Set simulation attributes
+    const float        object_spawn_delay    = 0.025f;
+    const float        object_spawn_speed    = 1200.0f;
+    const sf::Vector2f object_spawn_position = {500.0f, 200.0f};
+    const float        object_min_radius     = 1.0f;
+    const float        object_max_radius     = 20.0f;
+    const uint32_t     max_objects_count     = 1000;
+    const float        max_angle             = 1.0f;
 
-	bool paused = false;
+    sf::Clock clock;
+    // Main loop
+    while (window.isOpen()) {
+        sf::Event event{};
+        while (window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed || sf::Keyboard::isKeyPressed(sf::Keyboard::Escape)) {
+                window.close();
+            }
+        }
 
-	bool playerJustHitBall = false;
-	float CPUReactionTime = 0.5f;
-	float targetPos;
+        if (solver.getObjectsCount() < max_objects_count && clock.getElapsedTime().asSeconds() >= object_spawn_delay) {
+            clock.restart();
+            auto&       object = solver.addObject(object_spawn_position, RNGf::getRange(object_min_radius, object_max_radius));
+            const float t      = solver.getTime();
+            const float angle  = max_angle * sin(t) + Math::PI * 0.5f;
+            solver.setObjectVelocity(object, object_spawn_speed * sf::Vector2f{cos(angle), sin(angle)});
+            object.color = getRainbow(t);
+        }
 
-	sf::Texture Menu1_tex;
-	if(!Menu1_tex.loadFromFile("../../../start_menu1.png")){
-		std::cout<<"failed to load textr1"<<std::endl;
-	}
-	sf::Texture Menu2_tex; 
-	if(!Menu2_tex.loadFromFile("../../../start_menu2.png")){
-		std::cout<<"failed to load textr2"<<std::endl;
-	}
-
-	sf::Sprite Menu1;
-	Menu1.setTexture(Menu1_tex);
-	sf::Sprite Menu2;
-	Menu2.setTexture(Menu2_tex);
-
-    sf::Text p1Score_Display;
-	sf::Text p2Score_Display;
-
-    sf::Font font;
-    if(!font.loadFromFile("../../../ARIAL.TTF")){
-        std::cout << "dang that's crazy" << std::endl;
-    }
-    p1Score_Display.setFont(font);
-	p2Score_Display.setFont(font);
-
-    p1Score_Display.setString("0");
-	p2Score_Display.setString("0");
-
-    p1Score_Display.setCharacterSize(36);
-	p2Score_Display.setCharacterSize(36);
-
-	p1Score_Display.setPosition(800,50); //the scores are placed on the opposite side of their respective goal 
-	p2Score_Display.setPosition(150,50);
-
-    p1Score_Display.setFillColor(sf::Color::White);
-	p2Score_Display.setFillColor(sf::Color::White);
-
-	Mode gameMode; //determines whether it's 1 player or 2 player
-
-    //p1Score_Display.setStyle(sf::Text::Bold);
-
-	State gameState = player_press_enter;
-
-	while (window.isOpen()) //main render loop
-	{
-		sf::Time dt_time = deltaSrc.restart();
-		float dt = dt_time.asSeconds();
-
-		sf::Event event;
-		while (window.pollEvent(event))
-		{
-			if (event.type == sf::Event::Closed) //if closed event, then close window
-				window.close();
-		}
-
-		//IF PRE-ENTER, WAIT TO PRESS ENTER
-		if(gameState == player_press_enter){
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Enter)){
-				std::cout<<"PRESSED ENTER"<<std::endl;
-				gameState = player_num_choose;
-			}
-		}
-
-		//IF PRE-CHOOSE, WAIT TO CHOOSE
-		if(gameState == player_num_choose){
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num1)){
-				std::cout<<"PRESSED 1"<<std::endl;
-				gameMode = OnePlayerMode;
-				gameState = ball_prerelease;
-			}
-			if(sf::Keyboard::isKeyPressed(sf::Keyboard::Num2)){
-				std::cout<<"PRESSED 2"<<std::endl;
-				gameMode = TwoPlayerMode;
-				gameState = ball_prerelease;
-			}
-		}
-
-		//IF PRE-RELEASE STATE, RELEASE BALL
-		if(gameState == ball_prerelease){
-			ball.pos = ballPos;
-			//Determine Velocity X
-			if(p1Score <= p2Score){ //always serve to the lowest player or P1 in case of tie
-				ball.velocity.x = -200;
-			}else{
-				ball.velocity.x = 200;
-			}
-			//Determine Velocity Y
-			srand(rand());
-			int choice = rand() % 2;
-			if(choice == 0){
-				ball.velocity.y = 80;
-			}
-			else if (choice == 1){
-				ball.velocity.y = -80;
-			}
-			
-			std::cout << "LAUNCH VELOCITY: " << ball.velocity.y << std::endl;
-
-			//Reset Paddle Positions
-			paddle1.pos = paddle1_pos;
-			paddle2.pos = paddle2_pos;
-
-			gameState = ball_moving_regular_gameplay;
-		}
-
-		window.clear();
-
-		//P for pause
-		if (sf::Keyboard::isKeyPressed(sf::Keyboard::P)){
-			if(gameState == ball_moving_regular_gameplay){
-				if(!paused){
-					std::cout << "P to pause" << std::endl;
-					gameState = PAUSE;
-				}
-				paused = true;
-			}
-			else if (gameState == PAUSE){
-				std::cout << "level 1" << std::endl; 
-				if(!paused){
-					std::cout << "UNPAUSE" << std::endl;
-					gameState = ball_moving_regular_gameplay;
-				}
-				paused = true;
-			}
-		}else{
-			paused = false;
-		}
-
-		if(gameState == ball_moving_regular_gameplay){
-			//PLAYER INPUT-------------------------------------
-
-			//Player 1 Controller (up and down) ----- NO LIMITS
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::W) 
-			&& paddle1.pos.y > 140) {
-				paddle1.update(dt, -1);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)
-			&& paddle1.pos.y < 680) {
-				paddle1.update(dt, 1);
-			}
-
-			//Player 2 Controller (up and down) ----- NO LIMITS
-			if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up) 
-			&& gameMode == TwoPlayerMode
-			&& paddle2.pos.y > 140) {
-				paddle2.update(dt, -1);
-			}
-			else if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down) 
-			&& gameMode == TwoPlayerMode
-			&& paddle2.pos.y < 680) {
-				paddle2.update(dt, 1);
-			}
-
-			ball.update(dt);
-			
-			//CPU Controller
-			if(gameMode == OnePlayerMode){
-				if(playerJustHitBall){ //if the player hit the ball
-					targetPos = ball.pos.y; //PLUS SOME OFFSET
-					if(CPUReactionTime <= 0){ //if the CPU has finished waiting
-						if (paddle2.pos.y + 80 < targetPos){
-							paddle2.update(dt, 1);
-						}
-						else if (paddle2.pos.y + 10 > targetPos){
-							paddle2.update(dt, -1);
-						}
-					}
-					else{ //if the CPU needs to wait longer
-						CPUReactionTime -= dt;
-						if(CPUReactionTime <= 0){ //this should only trigger once every time the CPU needs to hit
-
-						}
-					}
-				}
-				else{ //if the player hasn't hit the ball 
-					CPUReactionTime = 0.32f;
-				}
-			}
-
-			//PADDLE COLLISION -> REVERSE X VELOCITY
-			int addVelocity = 25;
-			if (ball.getShape().getGlobalBounds().intersects(paddle1.getShape().getGlobalBounds()) || 
-				ball.getShape().getGlobalBounds().intersects(paddle2.getShape().getGlobalBounds())){
-				if(!paddleTouch){
-					//reverse x velocity if you hit bottom half (MAYBE you can divide getGlobalBounds and manually check??)
-					if(ball.getShape().getGlobalBounds().intersects(paddle1.getShape().getGlobalBounds())){
-						std::cout<< "Hit Paddle 1" << std::endl;
-						playerJustHitBall = true;
-					}
-					if(ball.getShape().getGlobalBounds().intersects(paddle2.getShape().getGlobalBounds())){
-						std::cout<< "Hit Paddle 2" << std::endl;
-						playerJustHitBall = false;
-					}
-					ball.velocity.x *= -1;
-					//reverse x and y velocity if you hit the top half
-
-					//speed up the ball
-					if(ball.velocity.x > 0){
-						ball.velocity.x += addVelocity;
-					} else{
-						ball.velocity.x -= addVelocity;
-					}
-					//slightly randomize the return y velocity 
-				}
-				paddleTouch = true; //these bools help to ensure that only 1 collision happens
-			}else{
-				paddleTouch = false;
-			}
-
-			//WALL COLLISION -> REVERSE Y VELOCITY
-			if ((ball.getShape().getGlobalBounds().intersects(downWall.getShape().getGlobalBounds())) || 
-				(ball.getShape().getGlobalBounds().intersects(upWall.getShape().getGlobalBounds()))){
-				if(!ceilingTouch){
-					std::cout<< "Hit Wall" << std::endl;
-					ball.velocity.y *= -1;
-				} 
-				ceilingTouch = true;
-			}else{
-				ceilingTouch = false;
-			}
-
-			//GOALPOST COLLISION -> SCORE INCREASED and BALL RESET
-			if (ball.getShape().getGlobalBounds().intersects(leftWall.getShape().getGlobalBounds())){ //left goal post
-				if(!leftWallTouch){
-					p1Score += 1;
-					p1Score_Display.setString(std::to_string(p1Score));
-					std::cout<< "P2 SCORE: " << p1Score << std::endl;
-					//--> then pause, flash, display score, and reposition ball
-					gameState = ball_hit_wall_so_flash;
-				} 
-				leftWallTouch = true;
-			}else{
-				leftWallTouch = false;
-			}		
-			if (ball.getShape().getGlobalBounds().intersects(rightWall.getShape().getGlobalBounds())){ //right goal post
-				if(!rightWallTouch){
-					p2Score += 1;
-					p2Score_Display.setString(std::to_string(p2Score));
-					std::cout<< "P1 SCORE: " << p2Score << std::endl;
-					//--> then pause, flash, display score, and reposition ball 
-					gameState = ball_hit_wall_so_flash;
-				}
-				rightWallTouch = true;
-			}else{
-				rightWallTouch = false;
-			}
-		}
-		
-
-		//DRAW CALLS-------------------------------------
-		
-
-		window.draw(ball.getShape());
-
-        window.draw(paddle1.getShape());
-        window.draw(paddle2.getShape());
-
-		window.draw(leftWall.getShape());
-        window.draw(rightWall.getShape());
-        window.draw(upWall.getShape());
-        window.draw(downWall.getShape());
-        
-        if(gameState == ball_moving_regular_gameplay){
-			window.draw(p1Score_Display);
-			window.draw(p2Score_Display);
-		}
-
-		if(gameState == ball_hit_wall_so_flash && flashTime > 0){
-			ball.pos = ballPos;
-			if(flashTime > 0.8){ //std::fmod(flashTime, 0.5f) == 1
-				window.draw(p1Score_Display);
-				window.draw(p2Score_Display);
-			}
-			else if (flashTime > 0.6){
-
-			}
-			else if (flashTime > 0.4){
-				window.draw(p1Score_Display);
-				window.draw(p2Score_Display);
-			}
-			else if (flashTime > 0.2){
-
-			}
-			flashTime -= dt;
-		}
-		else if(!(flashTime > 0)){
-			flashTime = 1.0f;
-			gameState = ball_prerelease;
-		}
-
-		if(gameState == player_press_enter){
-			window.draw(Menu1);
-		}
-		if(gameState == player_num_choose){
-			window.draw(Menu2);
-		}
-
+        solver.update();
+        window.clear(sf::Color::White);
+        renderer.render(solver);
 		window.display();
-	}
-	return 0;
+    }
+
+    return 0;
 }
